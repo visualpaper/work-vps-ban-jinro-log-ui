56589f3e7af108b0f9317e235dfdaaccbf1eb5df
visualpaper <kazuya.umejima@gmail.com>
2023-08-17 16:05:50 +0900

    User Context (#5)

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,10 @@ import { createHandler } from 'graphql-http/lib/use/express'
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
 import { loadSchemaSync } from '@graphql-tools/load'
 import { addResolversToSchema } from '@graphql-tools/schema'
+import { User } from './types/generated/types'
+
+// ログイン実施済みか
+var logined = true
 
 const sleep = (time: any) => {
   return new Promise<void>((resolve) => {
@@ -12,13 +16,27 @@ const sleep = (time: any) => {
   })
 }
 
-const schema = loadSchemaSync('schema.graphql', {
+const schema = loadSchemaSync('./schema/schema.graphql', {
   loaders: [new GraphQLFileLoader()],
 })
 
 const resolvers = {
   Query: {},
-  Mutation: {},
+  Mutation: {
+    initialize: async (_: unknown, args: {}): Promise<User | null> => {
+      var villageNumbers: number[] = []
+      console.log(args)
+      await sleep(1000)
+
+      if (logined) {
+        villageNumbers.push(1, 2, 3)
+      }
+      return {
+        id: 'bfd5677d-6ac6-49db-8904-bd65620785dd',
+        villageNumbers: villageNumbers,
+      }
+    },
+  },
 }
 
 // You can add resolvers to that schema
